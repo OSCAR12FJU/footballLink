8bc5ed6bc051adf9c3fe32f1929ceaea1356b5fd
Oscar Flores <oscar.blv.cabj@hotmail.com>
2025-06-06 12:08:02 -0300

    feat: synchronizes input state with player selection

--- src/components/soccerGame/SoccerGame.tsx
+++ src/components/soccerGame/SoccerGame.tsx
@@ -314,34 +314,19 @@ export default function SoccerGame(){
               ) : (
                 //Si toda via no se queda sin opciónes y no adivina sigue la opción para colocar las opciónes
                 <div className="flex items-center max-w-lg mx-auto gap-2">
-                  {/* <input
-                    type="text"
-                    value={guessInput}
-                    onChange={(e) => setGuessInput(e.target.value)}
-                    placeholder="Enter player name..."
-                    className="flex-1 px-4 py-2 rounded-md bg-[#0d2137] border border-gray-700 text-white"
-                  />
-                  <button
-                    className="searchButton"
-                    onClick={handleGuess}
-                  >
-                    Guess
-                  </button> */}
-
                 <div className="relative w-full">
                     <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                        {/* <svg className="w-4 h-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 21 21">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11.15 5.6h.01m3.337 1.913h.01m-6.979 0h.01M5.541 11h.01M15 15h2.706a1.957 1.957 0 0 0 1.883-1.325A9 9 0 1 0 2.043 11.89 9.1 9.1 0 0 0 7.2 19.1a8.62 8.62 0 0 0 3.769.9A2.013 2.013 0 0 0 13 18v-.857A2.034 2.034 0 0 1 15 15Z"/>
-                        </svg> */}
-                        <svg  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  className="w-6 h-6 text-gray-500  hover:text-gray-900 "><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" /><path d="M12 7l4.76 3.45l-1.76 5.55h-6l-1.76 -5.55z" /><path d="M12 7v-4m3 13l2.5 3m-.74 -8.55l3.74 -1.45m-11.44 7.05l-2.56 2.95m.74 -8.55l-3.74 -1.45" /></svg>
+                     <svg  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  className="w-6 h-6 text-gray-500  hover:text-gray-900 "><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" /><path d="M12 7l4.76 3.45l-1.76 5.55h-6l-1.76 -5.55z" /><path d="M12 7v-4m3 13l2.5 3m-.74 -8.55l3.74 -1.45m-11.44 7.05l-2.56 2.95m.74 -8.55l-3.74 -1.45" /></svg>
                     </div>
                     <input 
                     type="text" 
                     value={guessInput}
                     onChange={(e) => handleChange(e)}
-                    onFocus={(e) => {
-                    e.target.scrollIntoView({ behavior: "smooth", block: "center" });
-  }}
+                    // onFocus={(e) => {
+                    // e.target.scrollIntoView({ behavior: "smooth", block: "center" });
+                    // }}
+                    onTouchStart={(e) => {
+                    e.currentTarget.scrollIntoView({ behavior: "smooth", block: "center" });}}
                     onKeyDown={(e) =>{
                       if(e.key === "Enter") handleGuess();
                     }}
